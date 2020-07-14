3fac0ee5b997f2f9e4ec3594bed31fa2475174cd
rad6 <heydarirad_hadi@yahoo.com>
2020-07-14 19:36:54 +0430

    PaymentDetail and PaymentDetailGateway protos are added

--- PaymentDetail.py
+++ PaymentDetail.py
@@ -1,4 +1,19 @@
 
 class PaymentDetail:
-    pass
-    
+    def __init__(self, amount, status, tracking_code):
+        self.__amount = amount
+        self.__status = status
+        self.__tracking_code = tracking_code
+
+    def setAmount(self, value):
+        self.__amount = value
+
+    def pay(self):
+        # TODO
+        pass
+
+    def setStatus(self, value):
+        self.__status = value
+
+    def setTrackingCode(self, value):
+        self.__tracking_code = value
