3d1abce015016fbf022408bde95f4b27720c7729
rad6 <heydarirad_hadi@yahoo.com>
2020-07-14 17:31:55 +0430

    serviceTime prototypes added

--- ServiceTime.py
+++ ServiceTime.py
@@ -1,6 +1,32 @@
 from date import date
+from enum import Enum
 
+
+class DateType(Enum):
+    ordinary = 1
+    special = 2
+    
 
 class ServiceTime:
-    pass
-    
+    def __init__(self, date, accessibility, type):
+        self.__date = date
+        self.__accessibility = accessibility
+        self.__type = type
+
+    def getDate(self):
+        return self.__date
+
+    def setDate(self, value):
+        self.__date = value
+
+    def getAccessibility(self):
+        return self.__accessibility
+
+    def setAccessibility(self, value):
+        self.__accessibility = value
+
+    def getType(self):
+        return self.__type
+
+    def setType(self, value):
+        self.__type = value
