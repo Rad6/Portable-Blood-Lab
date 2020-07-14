4b86f6eb5290897a92dca8f644ea45d6f97d130e
plutocholia <kiarash.nrz77@gmail.com>
2020-07-14 17:09:28 +0430

    Test.py is done

--- Test.py
+++ Test.py
@@ -1,4 +1,17 @@
 
 class Test:
-    pass
+    def __init__ (self, name="no-name", mustinclucepresc=False):
+        self.__name = name
+        self.__mustIncluidePresc = mustinclucepresc
     
+    def getName(self):
+        return self.__name
+    
+    def setName(self, name):
+        self.__name = name
+    
+    def getMustInclucePresc(self):
+        return self.__mustIncluidePresc
+
+    def setMustIncludePresc(self, must):
+        self.__mustIncluidePresc = must    
