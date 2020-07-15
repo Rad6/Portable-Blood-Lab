b171a91d9126b9723745eb211b820770a7908625
plutocholia <kiarash.nrz77@gmail.com>
2020-07-15 09:33:48 +0430

    str is added

--- Location.py
+++ Location.py
@@ -3,3 +3,6 @@ class Location:
     def __init__(self, latitude, longitude):
         self.__latitude = latitude
         self.__longitude = longitude
+    
+    def __str__(self):
+        return f"Location({self.__latitude}, {self.__longitude})"
