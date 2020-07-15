e273c1d938c0801a747a0e9ec2f3ea860e5f12f0
plutocholia <kiarash.nrz77@gmail.com>
2020-07-15 09:35:57 +0430

    UI is added

--- UI.py
+++ UI.py
@@ -1,6 +1,37 @@
+from Handler import Handler
 
 
 class UI:
 
     def __init__(self):
-        pass
+        self.__handler = Handler()
+
+    def chooseTime(self):
+        total_times = self.__handler.getTimes()
+        
+        print("\t0 : back")
+        for i, item in enumerate(total_times):
+            print(f"\t{i+1} : {item}")
+        
+
+        while True:
+            print("Choose a time from times above: ", end="")
+            chosen_time = int(input())
+            if chosen_time >= 0 and chosen_time <= len(total_times):
+                break
+            else:
+                print("Try Again, ", end="")
+        
+        if chosen_time == 0:
+            # TODO: BACK TO PREV SEC
+            pass
+        else:
+            self.__handler.chooseTime(total_times[chosen_time-1])
+            print(f"the time is successfully added")
+            # TODO: back to the menu
+
+
+
+if __name__ == "__main__":
+    ui = UI()
+    ui.chooseTime()
