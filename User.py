b31b6cd0482d945abbe932e46d22a7d660faef09
plutocholia <kiarash.nrz77@gmail.com>
2020-07-16 09:32:44 +0430

    User inherits Person + str and username and some getter setters are added

--- User.py
+++ User.py
@@ -1,11 +1,21 @@
 from Prescription import Prescription
+from Person import Person
 
-class User:
-    def __init__(self, is_logged_in, password_hash, is_blocked, prescs = []):
+class User(Person):
+    def __init__(self, username, is_logged_in=False, \
+        password_hash="dfkjs984w9f09343", is_blocked=False, prescs = []):
+        self.__username = username
         self.__is_logged_in = is_logged_in
         self.__password_hash = password_hash
         self.__is_blocked = is_blocked
         self.__prescs = prescs
+        super().__init__()
+
+    def addPrescription(self, presc):
+        self.__prescs.append(presc)
+    
+    def setPrescriptions(self, prescriptions):
+        self.__prescs = prescriptions
 
     def getPrescriptions(self):
         return self.__prescs
@@ -13,4 +23,12 @@ class User:
     def createOrder(self, pd):
         # TODO
         pass
-
+    
+    def getUsername(self):
+        return self.__username
+    
+    def setUsername(self, username):
+        self.__username = username
+    
+    def __str__(self):
+        return f"User: {super().__str__()} username: {self.__username}"
